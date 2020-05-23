c0238dc7e12abab43c180ef499da8b7dc19dbfd1
lucassherwin <lksherwin@gmail.com>
2020-05-23 14:23:27 -0400

    added user captions to profile

--- caption-game-client/src/components/ProfilePage.js
+++ caption-game-client/src/components/ProfilePage.js
@@ -6,16 +6,19 @@ export default class ProfilePage extends Component {
     render(){
         return(
             <div>
+                <h1>This is the profile page</h1>
                 <div>
-                    <h1>This is the profile page</h1>
                     <h2>Posts</h2>
+                    <UserPost />
                 </div>
-
-                <UserPost />
-
                 <div>
                     <p>Username</p>
                     <p>User Bio</p>
+                    <p>User Profile</p>
+                </div>
+                <div>
+                    <h2>Captions</h2>
+                    <UserCaption />
                 </div>
             </div>
         )
